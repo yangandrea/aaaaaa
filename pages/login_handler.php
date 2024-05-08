442e5db7dab1ce044993668bd5f02e94455a710c
meucci <meucci@itismeucci.com>
2024-05-08 10:43:49 +0200

    mn

--- pages/login_handler.php
+++ pages/login_handler.php
@@ -6,15 +6,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Qui dovresti verificare le credenziali dell'utente nel tuo database
-    // Per semplicità, in questo esempio, supponiamo che le credenziali siano corrette
+    // Preparare la query SQL
+    $stmt = $conn->prepare("SELECT * FROM users WHERE username = ?");
+    $stmt->bind_param("s", $username);
 
-    $_SESSION['username'] = $username;
+    // Eseguire la query
+    $stmt->execute();
 
-    if ($username === 'admin') {
-        header('Location: admin_dashboard.php');
+    // Ottenere i risultati
+    $result = $stmt->get_result();
+    $user = $result->fetch_assoc();
+
+    // Verificare se l'utente esiste e la password è corretta
+    if ($user && password_verify($password, $user['password'])) {
+        // Login riuscito, impostare l'username in sessione e reindirizzare l'utente
+        $_SESSION['username'] = $username;
+
+        if ($username === 'admin') {
+            header('Location: admin_dashboard.php');
+        } else {
+            header('Location: negozio.php');
+        }
     } else {
-        header('Location: negozio.php');
+        echo 'Login failed, setting error message';
+        header('Location: ../index.php');
     }
 }
-?>
