4f0825a43b8b6844bede8dd16f31d703c3064cbe
itismeucci <yang.andrea@itismeucci.com>
2024-05-27 09:50:16 +0200

    a

--- pages/carello.php
+++ pages/carello.php
@@ -10,7 +10,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $result = $conn->query($sql);
         if ($result->num_rows > 0) {
             $product = $result->fetch_assoc();
-            $_SESSION['cart'][$product_id] = $product;
+            if (isset($_SESSION['cart'][$product_id])) {
+                $_SESSION['cart'][$product_id]['count'] += 1;
+            } else {
+                $product['count'] = 1;
+                $_SESSION['cart'][$product_id] = $product;
+            }
         }
     }
 }
@@ -27,14 +32,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <button type="button"><a href="../index.php">Homepage</a></button>
 </div>
 <h2 id="cartTitle">Carrello</h2>
+<p id="productCount" class="text-white">Prodotti nel carrello: <?php echo count($_SESSION['cart']); ?></p>
 <?php if (isset($_SESSION['cart']) && count($_SESSION['cart']) > 0): ?>
     <div class="container">
         <div class="row">
             <?php foreach ($_SESSION['cart'] as $product_id => $product_details): ?>
                 <div class="col-sm" id="productDiv<?php echo $product_id; ?>">
-                    <h2><?php echo $product_details['name']; ?></h2>
-                    <p id="productDescription<?php echo $product_id; ?>"><?php echo $product_details['description']; ?></p>
-                    <p id="productPrice<?php echo $product_id; ?>">Price: <?php echo $product_details['price']; ?></p>
+                    <h2 class="text-white"><?php echo $product_details['name']; ?></h2>
+                    <p id="productDescription<?php echo $product_id; ?>" class="text-white"><?php echo $product_details['description']; ?></p>
+                    <p id="productPrice<?php echo $product_id; ?>" class="text-white">Price: <?php echo $product_details['price']; ?></p>
+                    <p id="productCount<?php echo $product_id; ?>" class="text-white">Quantity: <?php echo $product_details['count']; ?></p>
                 </div>
             <?php endforeach; ?>
         </div>
